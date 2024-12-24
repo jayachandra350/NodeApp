e9bae0e3845048371b06529db293dd832f231bb1
jayachandra350 <chandrajaya795@gmail.com>
2024-12-24 19:48:36 +0530

    Update Deploy.groovy

--- Deploy.groovy
+++ Deploy.groovy
@@ -4,11 +4,11 @@ pipeline {
         AWS_REGION = 'ap-south-1' // Replace with your AWS region
         ECR_REPO = 'public.ecr.aws/v2k5k1u2/mynodeapp' // Replace with your ECR repository URI
         EC2_HOST = '13.201.69.101' // Replace with your EC2 public IP or hostname
-        DOCKER_IMAGE = 'latest' // Replace with your Docker image tag
+        DOCKER_TAG = 'latest' // Replace with your Docker image tag
         SSH_KEY_CREDENTIALS = 'ec2-ssh-key' // Jenkins credentials ID for the EC2 SSH private key
     }
     stages {
-        stage('Pull Docker Image on EC2') {
+        stage('Deploy Docker Image on EC2') {
             steps {
                 sshagent(credentials: [SSH_KEY_CREDENTIALS]) {
                     script {
@@ -20,13 +20,13 @@ pipeline {
                         aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REPO}
                         
                         # Pull the Docker image
-                        docker pull ${ECR_REPO}:${DOCKER_IMAGE}
+                        docker pull ${ECR_REPO}:${DOCKER_TAG}
 
                         # Stop the currently running container (if any)
                         docker stop my-app || true && docker rm my-app || true
 
                         # Run the new container
-                        docker run -d --name my-app -p 5050:5050 ${ECR_REPO}:${DOCKER_IMAGE}
+                        docker run -d --name my-app -p 5050:5050 ${ECR_REPO}:${DOCKER_TAG}
                         EOF
                         '''
                     }
